a0627e473c21439a1f83cf62fc5922f795bf631c
Plsr <christianpoplawski@gmail.com>
2018-03-14 15:09:42 +0100

    Save dailyGoal to AsyncStorage
    The current solution is not very practical, since we'd need to read from
    the storage over and over again. I'll implement Redux in the next step
    to keep all the writing away from the layouts.

--- scenes/settings-screen.js
+++ scenes/settings-screen.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, Button, AsyncStorage } from 'react-native';
+
+const STORAGE_KEY_GOAL = '@FeuchtStore:dailyGoal'
 
 class SettingsScreen extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { dailyGoal: 0 }
+  }
+
+  componentWillMount() {
+    let dailyGoal = this.getDailyGoalFromStorage()
+    dailyGoal.then((val) => {
+      console.log(val)
+      this.setState({
+        dailyGoal: val
+      })
+    })
+  }
+
+  getDailyGoalFromStorage = async () => {
+    try {
+      const value = await AsyncStorage.getItem(STORAGE_KEY_GOAL);
+      return value == null ? 0 : value
+    } catch (error) {
+      return 0
+    }
+  }
+
+  writeDailyGoalToStorage = async (value) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY_GOAL, value);
+    } catch (error) {
+      console.log(error) 
+    }
+  }
+
+  handleDailyGoalChange = (value) => {
+    let dailyGoal = this.writeDailyGoalToStorage(value)
+    dailyGoal.then(() => {
+      this.setState({ dailyGoal: value })
+    })
+  }
+
   render() {
     return (
       <View>
         <Text> Welcome to the Settings </Text>
+        <Text> Your goal</Text>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => this.handleDailyGoalChange(text)}
+          value={this.state.dailyGoal.toString()}
+        />
       </View>
     )
   }
