0082c98b3dcb9bb2eb330e22e160c134550b300d
Plsr <christianpoplawski@gmail.com>
2018-03-14 11:00:52 +0100

    Pretty print for amount drank
    Switching between ML an L when the amount is higher than 999 ML

--- components/hydration-progress.js
+++ components/hydration-progress.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 
 class HydrationProgress extends Component {
+  prettyPrintAmount = (amount) => {
+    return amount < 1000 ? `${amount} ML` : `${amount / 1000} L`
+  }
+
   render() {
+    const { drankToday } = this.props
+
     return (
-      <Text>You drank {this.props.drankToday} today!</Text>
+      <Text>You drank {this.prettyPrintAmount(drankToday)} today!</Text>
     );
   }
 }
