a8b9760d333b3502bfd06f26290c1cdd96b5361c
Plsr <christianpoplawski@gmail.com>
2018-04-14 17:46:16 +0200

    Forgot to check in changed colors file

--- helpers/colors.js
+++ helpers/colors.js
@@ -2,4 +2,6 @@ export const waterColor = '#D9EAFE'
 export const black = '#474F57'
 export const accentColor = '#C0315C'
 export const white = '#FFFFFF'
+export const blueishGrey = '#F6F8FB'
+export const grey = '#D5D5D5'
 export const shadowColor = "rgba(0,0,0,0.15)"
